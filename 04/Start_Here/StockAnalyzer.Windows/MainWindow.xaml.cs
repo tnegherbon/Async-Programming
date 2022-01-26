d652ffc5c297933a33231bb8307478c4aac13578
Thiago Negherbon <thiagonegherbon@outlook.com>
2022-01-26 17:27:31 -0300

    - Solution and project files update for Module 4.

--- 04/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ 04/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,174 +1,170 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using StockAnalyzer.Core.Domain;
+using StockAnalyzer.Windows.Services;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
-using Newtonsoft.Json;
-using StockAnalyzer.Core.Domain;
-using StockAnalyzer.Windows.Services;
 
 namespace StockAnalyzer.Windows
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        CancellationTokenSource cancellationTokenSource = null;
+		CancellationTokenSource cancellationTokenSource = null;
 
-        private async void Search_Click(object sender, RoutedEventArgs e)
-        {
-            #region Before loading stock data
-            var watch = new Stopwatch();
-            watch.Start();
-            StockProgress.Visibility = Visibility.Visible;
-            StockProgress.IsIndeterminate = true;
+		private async void Search_Click(object sender, RoutedEventArgs e)
+		{
+			#region Before loading stock data
+			var watch = new Stopwatch();
+			watch.Start();
+			StockProgress.Visibility = Visibility.Visible;
+			StockProgress.IsIndeterminate = true;
 
-            Search.Content = "Cancel";
-            #endregion
+			Search.Content = "Cancel";
+			#endregion
 
-            #region Cancellation
-            if (cancellationTokenSource != null)
-            {
-                cancellationTokenSource.Cancel();
-                cancellationTokenSource = null;
-                return;
-            }
+			#region Cancellation
+			if (cancellationTokenSource != null)
+			{
+				cancellationTokenSource.Cancel();
+				cancellationTokenSource = null;
+				return;
+			}
 
-            cancellationTokenSource = new CancellationTokenSource();
+			cancellationTokenSource = new CancellationTokenSource();
 
-            cancellationTokenSource.Token.Register(() =>
-            {
-                Notes.Text += "Cancellation requested" + Environment.NewLine;
-            });
-            #endregion
+			cancellationTokenSource.Token.Register(() =>
+			{
+				Notes.Text += "Cancellation requested" + Environment.NewLine;
+			});
+			#endregion
 
-            try
-            {
-                #region Load One or Many Tickers
-                var tickers = Ticker.Text.Split(',', ' ');
+			try
+			{
+				#region Load One or Many Tickers
+				var tickers = Ticker.Text.Split(',', ' ');
 
-                var service = new StockService();
+				var service = new StockService();
 
-                var tickerLoadingTasks = new List<Task<IEnumerable<StockPrice>>>();
-                foreach (var ticker in tickers)
-                {
-                    var loadTask = service.GetStockPricesFor(ticker, cancellationTokenSource.Token);
+				var tickerLoadingTasks = new List<Task<IEnumerable<StockPrice>>>();
+				foreach (var ticker in tickers)
+				{
+					var loadTask = service.GetStockPricesFor(ticker, cancellationTokenSource.Token);
 
-                    tickerLoadingTasks.Add(loadTask);
-                }
-                #endregion
+					tickerLoadingTasks.Add(loadTask);
+				}
+				#endregion
 
-                var loadedStocks = await Task.WhenAll(tickerLoadingTasks);
+				var loadedStocks = await Task.WhenAll(tickerLoadingTasks);
 
-                
-                Stocks.ItemsSource = loadedStocks.SelectMany(stocks => stocks);
-            }
-            catch (Exception ex)
-            {
-                Notes.Text += ex.Message + Environment.NewLine;
-            }
-            finally
-            {
-                cancellationTokenSource = null;
-            }
 
-            #region After stock data is loaded
-            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
-            StockProgress.Visibility = Visibility.Hidden;
-            Search.Content = "Search";
-            #endregion
-        }
+				Stocks.ItemsSource = loadedStocks.SelectMany(stocks => stocks);
+			}
+			catch (Exception ex)
+			{
+				Notes.Text += ex.Message + Environment.NewLine;
+			}
+			finally
+			{
+				cancellationTokenSource = null;
+			}
 
-        private Task<List<string>> SearchForStocks(CancellationToken cancellationToken)
-        {
-            var loadLinesTask = Task.Run(async () =>
-            {
-                var lines = new List<string>();
+			#region After stock data is loaded
+			StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+			StockProgress.Visibility = Visibility.Hidden;
+			Search.Content = "Search";
+			#endregion
+		}
 
-                using (var stream = new StreamReader(File.OpenRead(@"StockPrices_small.csv")))
-                {
-                    string line;
-                    while ((line = await stream.ReadLineAsync()) != null)
-                    {
-                        if (cancellationToken.IsCancellationRequested)
-                        {
-                            return lines;
-                        }
-                        lines.Add(line);
-                    }
-                }
+		private Task<List<string>> SearchForStocks(CancellationToken cancellationToken)
+		{
+			var loadLinesTask = Task.Run(async () =>
+			{
+				var lines = new List<string>();
 
-                return lines;
-            }, cancellationToken);
+				using (var stream = new StreamReader(File.OpenRead(@"StockPrices_small.csv")))
+				{
+					string line;
+					while ((line = await stream.ReadLineAsync()) != null)
+					{
+						if (cancellationToken.IsCancellationRequested)
+						{
+							return lines;
+						}
+						lines.Add(line);
+					}
+				}
 
-            return loadLinesTask;
-        }
+				return lines;
+			}, cancellationToken);
 
-        #region Helpers
-        Random random = new Random();
-        private decimal CalculateExpensiveComputation(IEnumerable<StockPrice> stocks)
-        {
-            Thread.Yield();
+			return loadLinesTask;
+		}
 
-            var computedValue = 0m;
+		#region Helpers
+		Random random = new Random();
+		private decimal CalculateExpensiveComputation(IEnumerable<StockPrice> stocks)
+		{
+			Thread.Yield();
 
-            foreach (var stock in stocks)
-            {
-                for (int i = 0; i < stocks.Count() - 2; i++)
-                {
-                    for (int a = 0; a < random.Next(50, 60); a++)
-                    {
-                        computedValue += stocks.ElementAt(i).Change + stocks.ElementAt(i + 1).Change;
-                    }
-                }
-            }
+			var computedValue = 0m;
 
-            return computedValue;
-        }
+			foreach (var stock in stocks)
+			{
+				for (int i = 0; i < stocks.Count() - 2; i++)
+				{
+					for (int a = 0; a < random.Next(50, 60); a++)
+					{
+						computedValue += stocks.ElementAt(i).Change + stocks.ElementAt(i + 1).Change;
+					}
+				}
+			}
 
-        private decimal Compute(StockPrice stock)
-        {
-            Thread.Yield();
+			return computedValue;
+		}
 
-            decimal x = 0;
-            for (var a = 0; a < 10; a++)
-            {
-                for (var b = 0; b < 20; b++)
-                {
-                    x += a + stock.Change;
-                }
-            }
+		private decimal Compute(StockPrice stock)
+		{
+			Thread.Yield();
 
-            return x;
-        }
-        #endregion
+			decimal x = 0;
+			for (var a = 0; a < 10; a++)
+			{
+				for (var b = 0; b < 20; b++)
+				{
+					x += a + stock.Change;
+				}
+			}
 
-        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+			return x;
+		}
+		#endregion
 
-            e.Handled = true;
-        }
+		private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+		{
+			Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
 
-        private void Close_OnClick(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-    }
+			e.Handled = true;
+		}
 
-    class StockCalculation
-    {
-        public string Ticker { get; set; }
-        public decimal Result { get; set; }
-    }
+		private void Close_OnClick(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+
+	class StockCalculation
+	{
+		public string Ticker { get; set; }
+		public decimal Result { get; set; }
+	}
 }
